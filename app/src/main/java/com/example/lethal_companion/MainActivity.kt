240ff2d00b7d22b4a8905f776a9cda90135ebe74
Mori656 <marcelnosek@o2.pl>
2024-03-28 14:42:02 +0100

    Base menu
    Dodano podstawowe menu dzięki któremu można przemieszczać się między poszczególnymi activity

--- app/src/main/java/com/example/lethal_companion/MainActivity.kt
+++ app/src/main/java/com/example/lethal_companion/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.lethal_companion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.lethal_companion.bestiary.Bestiary
+import com.example.lethal_companion.items.Items
+import com.example.lethal_companion.logs.Logs
+import com.example.lethal_companion.minigame.Minigame
+import com.example.lethal_companion.tips.Tips
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val activitybuttontable = arrayOf(R.id.but_bestiary,R.id.but_items,R.id.but_logs,R.id.but_tips,R.id.but_minigame);
+        val classtable = arrayOf(Bestiary::class.java,Items::class.java,Logs::class.java,Tips::class.java,Minigame::class.java);
+
+        for (i  in 0 until activitybuttontable.size){
+            val buttonClick = findViewById<Button>(activitybuttontable[i])
+            buttonClick.setOnClickListener {
+                val intent = Intent(this, classtable[i])
+                startActivity(intent)
+            }
+        }
+
     }
 }
