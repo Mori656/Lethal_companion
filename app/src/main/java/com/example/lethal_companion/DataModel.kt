9b6aaca4bc7ed91c336280adc7ca5e8ff9c2beb8
MikolajNiewola <mikolaj.niewola.321@gmail.com>
2024-04-03 11:48:52 +0200

    retrofit

---
+++ app/src/main/java/com/example/lethal_companion/DataModel.kt
@@ -0,0 +1,43 @@
+package com.example.lethal_companion
+data class ResponseModel(
+    val Monsters: List<ElementMonsters>,
+    val Store: List<ElementStore>,
+    val Logs: List<ElementLogs>,
+    val Tips: List<ElementTips>,
+    val Game: List<ElementGame>
+)
+
+data class ElementMonsters(
+    val id: Int,
+    val name: String,
+    val dangerLevel: String,
+    val sName: String,
+    val desc: String,
+    val img: String
+)
+
+data class ElementStore(
+    val id: Int,
+    val name: String,
+    val price: Int,
+    val desc: String,
+    val img: String
+)
+
+data class ElementLogs(
+    val id: Int,
+    val name: String,
+    val desc: String,
+    val img: String
+)
+
+data class ElementTips(
+    val id: Int,
+    val name: String,
+    val desc: String
+)
+
+data class ElementGame(
+    val name: String,
+    val hiScore: Int
+)
